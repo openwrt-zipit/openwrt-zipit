2b9e11d89cef4c7c18946a3fe90d3bfceeb474a2
Angus Gratton <angus@projectgus.com>
2011-08-27 11:37:17 +1000

    Remove some default router packages not necessary for Z2

--- target/linux/pxa/zipitz2/target.mk
+++ target/linux/pxa/zipitz2/target.mk
@@ -5,7 +5,13 @@ define Target/Description
 endef
 
 FEATURES:=targz
+# packages we want for functional wifi support
 DEFAULT_PACKAGES += kmod-libertas-spi hostapd wpa-supplicant
+
+# default packages we don't probably want (router stuff like, NAT, PPP, DHCP/DNS servers, etc.)
+DEFAULT_PACKAGES += -iptables-mod-conntrack -iptables-mod-nat -ppp -ppp-mod-pppoe -libip4tc -libxtables -kmod-ipt-conntrack -kmod-ipt-nat -kmod-ipt-nathelper -kmod-ppp -kmod-pppoe -dnsmasq
+
+# Z2 console support
 DEPENDS := +@BUSYBOX_CONFIG_LOADFONT +@BUSYBOX_CONFIG_LOADKMAP +@BUSYBOX_CONFIG_FEATURE_LOADFONT_PSF2
 
 
