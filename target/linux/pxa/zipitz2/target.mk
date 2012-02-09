aa3fb20409e9b2aaa02b85384921c431c15bf7ef
Vasily Khoruzhick <anarsoul@gmail.com>
2012-02-09 22:39:27 +0300

    Zipit Z2: update base-files and target.mk
    * add sound and udc modules, gmenu2x to default packages,
    * load font and keymap from inittab
    * inittab: use gmenu2x as shell, fallback to /bin/sh if gmenu2x is not present
    * profile.d: setup LANG properly
    
    Signed-off-by: Vasily Khoruzhick <anarsoul@gmail.com>

--- target/linux/pxa/zipitz2/target.mk
+++ target/linux/pxa/zipitz2/target.mk
@@ -5,14 +5,11 @@ define Target/Description
 endef
 
 FEATURES:=targz usb usbgadget
-# packages we want for functional wifi support
-DEFAULT_PACKAGES += kmod-libertas-spi hostapd wpa-supplicant
+# packages we want for functional gui, wifi, sound, udc support
+DEFAULT_PACKAGES += gmenu2x kmod-libertas-spi kmod-sound-zipit-z2 kmod-pxa27x-udc hostapd wpa-supplicant
 
 # default packages we don't probably want (router stuff like, NAT, PPP, DHCP/DNS servers, etc.)
 DEFAULT_PACKAGES += -iptables-mod-conntrack -iptables-mod-nat -ppp -ppp-mod-pppoe -libip4tc -libxtables -kmod-ipt-conntrack -kmod-ipt-nat -kmod-ipt-nathelper -kmod-ppp -kmod-pppoe -dnsmasq
 
 # Z2 console support
 DEPENDS := +@BUSYBOX_CONFIG_LOADFONT +@BUSYBOX_CONFIG_LOADKMAP +@BUSYBOX_CONFIG_FEATURE_LOADFONT_PSF2
-
-
-
