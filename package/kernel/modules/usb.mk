bf977d26a3c992c631ca49d6fe1cc4788ccc2ede
Vasily Khoruzhick <anarsoul@gmail.com>
2012-02-25 13:26:24 +0300

    Disable EEM for usb gadget
    Signed-off-by: Vasily Khoruzhick <anarsoul@gmail.com>

--- package/kernel/modules/usb.mk
+++ package/kernel/modules/usb.mk
@@ -62,7 +62,7 @@ define KernelPackage/usb-eth-gadget
   KCONFIG:= \
 	CONFIG_USB_ETH \
 	CONFIG_USB_ETH_RNDIS=y \
-	CONFIG_USB_ETH_EEM=y
+	CONFIG_USB_ETH_EEM=n
   DEPENDS:=+kmod-usb-gadget
   FILES:=$(LINUX_DIR)/drivers/usb/gadget/g_ether.ko
   AUTOLOAD:=$(call AutoLoad,52,g_ether)
